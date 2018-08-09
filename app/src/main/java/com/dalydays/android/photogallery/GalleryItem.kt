1f496adea88042ff6b84db337cb4d19088a289a5
linucksrox <ericdalyblog@dalydays.com>
2018-08-09 12:51:30 -0400

    Add GalleryItem class, page 1692

---
+++ app/src/main/java/com/dalydays/android/photogallery/GalleryItem.kt
@@ -0,0 +1,8 @@
+package com.dalydays.android.photogallery
+
+data class GalleryItem(val caption: String, val id: String, val url: String) {
+
+    override fun toString(): String {
+        return caption
+    }
+}
