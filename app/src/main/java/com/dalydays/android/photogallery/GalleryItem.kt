502258220a7708d4b7c03ede9bc361f4e256f2bf
linucksrox <ericdalyblog@dalydays.com>
2018-09-04 16:23:33 -0400

    Looks like Gson can ignore json values with no matching properties

--- app/src/main/java/com/dalydays/android/photogallery/GalleryItem.kt
+++ app/src/main/java/com/dalydays/android/photogallery/GalleryItem.kt
@@ -1,17 +1,8 @@
 package com.dalydays.android.photogallery
 
 data class GalleryItem(var id: String,
-                       var owner: String,
-                       var secret: String,
-                       var server: String,
-                       var farm: Int,
                        var title: String,
-                       var ispublic: Int,
-                       var isfriend: Int,
-                       var isfamily: Int,
-                       var url_s: String,
-                       var height_s: String,
-                       var width_s: String) {
+                       var url_s: String) {
 
     override fun toString(): String {
         return title
